dd832c71e0c0dd082975c3a021c9a776b740535d
Ariel Barclay <mrsbarclay101008@gmail.com>
2020-07-13 12:41:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ require 'pry'
 def my_collect(students)
   yield
 end
-binding pry
+
 my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) { |student| student.split(" ").first}
          
          
