27352f9a0609dc1abd8b2919f9c71098fc5b4a33
Ariel Barclay <mrsbarclay101008@gmail.com>
2020-07-13 12:40:41 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -10,4 +10,4 @@ my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) { |student| student.split
 def my_collect(languages)
   yield
 end
-my_collect(languages) { |language| language.upcase }
+my_collect(['ruby', 'javascript', 'python', 'objective-c']) { |language| language.upcase }
