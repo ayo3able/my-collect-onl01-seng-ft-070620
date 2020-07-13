05ec49e9bc39a29cd1f5a473c50bb71d15b55fac
Ariel Barclay <mrsbarclay101008@gmail.com>
2020-07-13 12:47:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,6 @@ def my_collect(students)
   names =[]
   
   names << yield(students[i])
-  students
 end
 
 my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) { |student| student.split(" ").first}
